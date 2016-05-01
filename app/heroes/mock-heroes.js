2bf4797f72d885a6a3e6e84f2f89e8ee8968cc85
Paige Bolduc <paigebolduc@gmail.com>
2016-05-01 13:57:42 -0400

    add color property to hero class; enable users to change the color property and thereby modify the display color of the hero

--- app/heroes/mock-heroes.js
+++ app/heroes/mock-heroes.js
@@ -6,16 +6,16 @@ System.register([], function(exports_1, context_1) {
         setters:[],
         execute: function() {
             exports_1("HEROES", HEROES = [
-                { "id": 1, "name": "Mr. Nice" },
-                { "id": 2, "name": "Narco" },
-                { "id": 3, "name": "Bombasto" },
-                { "id": 4, "name": "Celeritas" },
-                { "id": 5, "name": "Magneta" },
-                { "id": 6, "name": "RubberMan" },
-                { "id": 7, "name": "Dynama" },
-                { "id": 8, "name": "Dr IQ" },
-                { "id": 9, "name": "Magma" },
-                { "id": 10, "name": "Tornado" }
+                { "id": 1, "name": "Mr. Nice", "color": "MistyRose" },
+                { "id": 2, "name": "Narco", "color": "AliceBlue" },
+                { "id": 3, "name": "Bombasto", "color": "PaleGreen" },
+                { "id": 4, "name": "Celeritas", "color": "Coral" },
+                { "id": 5, "name": "Magneta", "color": "Lavender" },
+                { "id": 6, "name": "RubberMan", "color": "RosyBrown" },
+                { "id": 7, "name": "Dynama", "color": "SlateBlue" },
+                { "id": 8, "name": "Dr IQ", "color": "Crimson" },
+                { "id": 9, "name": "Magma", "color": "MediumAquaMarine" },
+                { "id": 10, "name": "Tornado", "color": "LightCoral " }
             ]);
         }
     }
--- app/heroes/mock-heroes.ts
+++ app/heroes/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export var HEROES: Hero[] = [
-	{"id": 1, "name": "Mr. Nice"},
-	{"id": 2, "name": "Narco"},
-	{"id": 3, "name": "Bombasto"},
-	{"id": 4, "name": "Celeritas"},
-	{"id": 5, "name": "Magneta"},
-	{"id": 6, "name": "RubberMan"},
-	{"id": 7, "name": "Dynama"},
-	{"id": 8, "name": "Dr IQ"},
-	{"id": 9, "name": "Magma"},
-	{"id": 10, "name": "Tornado"}
+	{"id": 1, "name": "Mr. Nice", "color": "MistyRose"},
+	{"id": 2, "name": "Narco",  "color": "AliceBlue"},
+	{"id": 3, "name": "Bombasto", "color": "PaleGreen"},
+	{"id": 4, "name": "Celeritas", "color": "Coral"},
+	{"id": 5, "name": "Magneta", "color": "Lavender" },
+	{"id": 6, "name": "RubberMan", "color": "RosyBrown"},
+	{"id": 7, "name": "Dynama", "color": "SlateBlue"},
+	{"id": 8, "name": "Dr IQ", "color": "Crimson"},
+	{"id": 9, "name": "Magma", "color": "MediumAquaMarine"},
+	{"id": 10, "name": "Tornado", "color": "LightCoral "}
 ];
